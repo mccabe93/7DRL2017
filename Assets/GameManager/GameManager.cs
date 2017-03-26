14a341cfa5345d87cb83cb9eac9f2c85c6df194c
Braxton Lancial <BraxtonLanc@gmail.com>
2017-03-25 22:12:02 -0500

    Only Pause menu functionality right now.

---
+++ Assets/GameManager/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject pauseMenu;
+    public bool isPaused = false;
+
+    // Use this for initialization
+    void Start() {
+       pauseMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        //If pres Escape, then pause
+        if (Input.GetKeyDown(KeyCode.Escape) && isPaused == false)
+        {
+            toPauseMenu();
+        }
+
+    }
+
+    /*
+    * Restarts the current level
+    */
+    public void toRestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    /*
+    * Brings up the pause menu
+    */
+    public void toPauseMenu()
+    {
+        isPaused = true;
+        pauseMenu.SetActive(true);
+    }
+
+    /*
+    * Takes you to the main menu screen
+    */
+    public void toMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    /*
+    * Resumes the game from a paused context
+    */
+    public void toResumeGame()
+    {
+        isPaused = false;
+        pauseMenu.SetActive(false);
+    }
+
+    /*
+    * Quits the game
+    */
+    public void toQuit()
+    {
+        Application.Quit();
+    }
+}
