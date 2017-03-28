0905c0108ecd73f066ebcf229d2a0e13828f3b1b
Braxton Lancial <BraxtonLanc@gmail.com>
2017-03-27 22:39:21 -0500

    Commit of MainMenu related items.
    One script. One prefab for UI (with several children). Bunch of art from freegameart

---
+++ Assets/MainMenu/MainMenuManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /*
+    * Takes to first level
+    */
+    public void toGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    /*
+    * Quits the game
+    */
+    public void toQuit()
+    {
+        Application.Quit();
+    }
+}
