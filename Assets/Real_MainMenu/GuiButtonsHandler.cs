aeeb74e7ea528a3fa1d209b702e88e39bf599224
Braxton Lancial <BraxtonLanc@gmail.com>
2017-03-29 23:36:56 -0500

    Main Menu with Animation movie and music.

---
+++ Assets/Real_MainMenu/GuiButtonsHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GuiButtonsHandler : MonoBehaviour, IPointerClickHandler,
+     IPointerDownHandler,
+     IPointerUpHandler,
+     IPointerEnterHandler,
+     IPointerExitHandler,
+     ISelectHandler
+{ 
+
+    private Color origColor;
+    private Color origEffect;
+    private Color purpley = new Color(139f/255f, 31f/255f, 194f/255f, 255f/255f);
+    public Text myText;
+    private Outline outline;
+
+    void Start()
+    {
+        outline = myText.GetComponent<Outline>();
+        origColor = myText.color;
+        origEffect = outline.effectColor;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        myText.color = purpley;
+        outline.effectColor = Color.white;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        myText.color = origColor;
+        outline.effectColor = origEffect;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Assets/Real_MainMenu/MovieHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MovieHandler : MonoBehaviour {
+
+    public MovieTexture movie;
+    public AudioSource audio;
+    public AudioClip a_clip;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+        audio = GetComponent<AudioSource>();
+        audio.clip = a_clip;
+        movie.Play();
+        audio.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
